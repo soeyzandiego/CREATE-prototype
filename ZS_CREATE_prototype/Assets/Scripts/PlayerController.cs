b42dcaaaf2689c6f0e8f6e8f58ff75603b33f6cd
soeyzandiego <59988129+soeyzandiego@users.noreply.github.com>
2024-03-24 21:27:35 -0400

    Prototype 1 lessons complete (unit 1)

--- ZS_CREATE_prototype/Assets/Scripts/PlayerController.cs
+++ ZS_CREATE_prototype/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed = 20f;
+    [SerializeField] float turnSpeed = 30f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +16,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float forwardInput = Input.GetAxis("Vertical");
+
         // move vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // turn vehicle
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
