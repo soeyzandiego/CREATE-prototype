8eb4a2a3c8e7d85c68aef32f8746c37af60b580d
soeyzandiego <59988129+soeyzandiego@users.noreply.github.com>
2024-03-30 14:49:07 -0400

    Unit 3 completed, unit 4 assets imported

--- ZS_CREATE_prototype/Assets/Scripts/Prototype 3/PlayerControllerTHREE.cs
+++ ZS_CREATE_prototype/Assets/Scripts/Prototype 3/PlayerControllerTHREE.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class PlayerControllerTHREE : MonoBehaviour
 {
     [SerializeField] float jumpForce = 8f;
-    [SerializeField] float gravityModifier; 
+    [SerializeField] float gravityModifier;
 
+    [Header("Particles")]
+    [SerializeField] ParticleSystem explosionParticle;
+    [SerializeField] ParticleSystem dirtParticle;
+
+    [Header("Audio")]
+    [SerializeField] AudioClip jumpSound;
+    [SerializeField] AudioClip crashSound;
+
+    // component references
     Rigidbody rb;
+    Animator anim;
+    AudioSource audioSource;
+
     bool grounded;
     bool gameOver = false;
 
@@ -15,23 +27,42 @@ public class PlayerControllerTHREE : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        if (Input.GetKeyDown(KeyCode.Space) && grounded && !gameOver)
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             grounded = false;
+            anim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            audioSource.PlayOneShot(jumpSound);
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Ground")) { grounded = true; }
-        else if (collision.gameObject.CompareTag("Obstacle")) { gameOver = true; Debug.Log("Game Over"); }
+        if (collision.gameObject.CompareTag("Ground")) 
+        { 
+            grounded = true;
+            dirtParticle.Play();
+        }
+        else if (collision.gameObject.CompareTag("Obstacle")) 
+        { 
+            gameOver = true; 
+            Debug.Log("Game Over");
+            anim.SetBool("Death_b", true);
+            anim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            audioSource.PlayOneShot(crashSound);
+        }
     }
 
     public bool GameOver() { return gameOver; }
